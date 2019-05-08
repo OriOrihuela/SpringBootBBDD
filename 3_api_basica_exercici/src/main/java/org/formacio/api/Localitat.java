9c7983ffe3f687c561823b68d8fb9040451ff920
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-08 21:43:17 +0200

    1st test case passed

--- 3_api_basica_exercici/src/main/java/org/formacio/api/Localitat.java
+++ 3_api_basica_exercici/src/main/java/org/formacio/api/Localitat.java
@@ -1,38 +1,56 @@
 package org.formacio.api;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Introduiu, sense canviar el codi ja existent, les annotacions de mapping necessaries
  * per tal que aquesta classe sigui una entitat mapejada a la taula T_LOCALITATS tal com
  * esta definida a:
  * src/main/resources/schema.sql
  */
+@Entity
+@Table(name = "T_LOCALITATS")
 public class Localitat {
 
-	private Long id;
-	
-	private String nom;
+    /* ---- Properties ---- */
+    @Id
+    @Column(name = "LOC_ID")
+    private Long id;
 
-	private Integer habitants;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public Integer getHabitants() {
-		return habitants;
-	}
-	public void setHabitants(Integer habitants) {
-		this.habitants = habitants;
-	}
-	
-	
-	
+    @Column(name = "LOC_NOM")
+    private String nom;
+
+    @Column(name = "LOC_HABS")
+    private Integer habitants;
+
+
+    /* ---- Getters ---- */
+    public Long getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public Integer getHabitants() {
+        return habitants;
+    }
+
+
+    /* ---- Setters ---- */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setHabitants(Integer habitants) {
+        this.habitants = habitants;
+    }
 }
--- 3_api_basica_exercici/src/test/java/org/formacio/api/JpaBasicApplicationTests.java
+++ 3_api_basica_exercici/src/test/java/org/formacio/api/JpaBasicApplicationTests.java
@@ -16,95 +16,95 @@ import org.springframework.test.context.junit4.SpringRunner;
 @Sql("classpath:/insert.sql")
 public class JpaBasicApplicationTests {
 
-	@Autowired(required=false)
-	private LocalitatOpBasic operacions;
-	
-	@PersistenceContext
-	private EntityManager em;
-	
-	/**
-	 * Observa (sense tocar!) l'estructura de la taula per guardar localitats a:
-	 * src/main/resources/schema.sql
-	 * 
-	 * Modifica la classe Localitat per tal que es correspongui a la estructura de la taula
-	 */
-	@Test
-	public void test_mapping() {
-		em.find(Localitat.class, 1L);
-	}
-	
-	/* ------------------------------
-	 * Les modificacions que el test demana a partir d'aqui es refereixen (a no ser que 
-	 * es digui d'altre manera) a la classe LocalitatOpBasic
-	 */
-	
-	/**
-	 * Modifica la classe LocalitatOpBasic per a que sigui localitzada com a Component per Spring
-	 */
-	@Test
-	public void test_component() {
-		Assert.assertNotNull(operacions);
-	}
-	
-	/**
-	 * Implementa el metode carrega 
-	 */
-	@Test
-	public void test_load() {
-		Localitat selva = operacions.carrega(1L);
+    @Autowired(required = false)
+    private LocalitatOpBasic operacions;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    /**
+     * Observa (sense tocar!) l'estructura de la taula per guardar localitats a:
+     * src/main/resources/schema.sql
+     * <p>
+     * Modifica la classe Localitat per tal que es correspongui a la estructura de la taula
+     */
+    @Test
+    public void test_mapping() {
+        em.find(Localitat.class, 1L);
+    }
+
+    /* ------------------------------
+     * Les modificacions que el test demana a partir d'aqui es refereixen (a no ser que
+     * es digui d'altre manera) a la classe LocalitatOpBasic
+     */
+
+    /**
+     * Modifica la classe LocalitatOpBasic per a que sigui localitzada com a Component per Spring
+     */
+    @Test
+    public void test_component() {
+        Assert.assertNotNull(operacions);
+    }
+
+    /**
+     * Implementa el metode carrega
+     */
+    @Test
+    public void test_load() {
+        Localitat selva = operacions.carrega(1L);
         Assert.assertNotNull(selva);
         Assert.assertEquals("Selva", selva.getNom());
         Assert.assertEquals(1000, selva.getHabitants().intValue());
-	}
-	
-	/**
-	 * Implementa el metode alta per donar d'alta noves localitats
-	 * Important: fitxau-vos que no passem l'id. L'id l'ha de generar la base de dades
-	 */
-	@Test
-	public void test_alta() {
-		operacions.alta("nova",2000);
-		Localitat alta = operacions.carrega(2L);
-		Assert.assertNotNull(alta);
-		Assert.assertEquals("nova", alta.getNom());
-		Assert.assertEquals(2000, alta.getHabitants().intValue());
-	}
-	
-	/**
-	 * Implementa el metode elimina per tal que elimini la localitat
-	 * amb id passat com a parametre
-	 */
-	@Test
-	public void test_elimina() {
-		Assert.assertNotNull(operacions.carrega(1L));
-		operacions.elimina(1L);
-		Assert.assertNull(operacions.carrega(1L));
-	}
-	
-	/**
-	 * Modifica el metode elimina per a que no falli si l'id passat
-	 * no existeix
-	 */
-	@Test
-	public void test_elimina_si_existeix() {
-		Assert.assertNotNull(operacions.carrega(1L));
-		operacions.elimina(1L);
-		Assert.assertNull(operacions.carrega(1L));
-		operacions.elimina(1L);
-	}
-	
-	/**
-	 * Implementa el metode modifica per a que guardi les modificacions 
-	 * de  l'objecte  passat com a parametre
-	 */
-	@Test
-	public void test_modifica() {
-		Localitat modificada = new Localitat();
-		modificada.setId(1L);
-		modificada.setNom("modificada");
-		
-		operacions.modifica(modificada);
-		
-		Assert.assertEquals("modificada", operacions.carrega(1L).getNom());
-	}
+    }
+
+    /**
+     * Implementa el metode alta per donar d'alta noves localitats
+     * Important: fitxau-vos que no passem l'id. L'id l'ha de generar la base de dades
+     */
+    @Test
+    public void test_alta() {
+        operacions.alta("nova", 2000);
+        Localitat alta = operacions.carrega(2L);
+        Assert.assertNotNull(alta);
+        Assert.assertEquals("nova", alta.getNom());
+        Assert.assertEquals(2000, alta.getHabitants().intValue());
+    }
+
+    /**
+     * Implementa el metode elimina per tal que elimini la localitat
+     * amb id passat com a parametre
+     */
+    @Test
+    public void test_elimina() {
+        Assert.assertNotNull(operacions.carrega(1L));
+        operacions.elimina(1L);
+        Assert.assertNull(operacions.carrega(1L));
+    }
+
+    /**
+     * Modifica el metode elimina per a que no falli si l'id passat
+     * no existeix
+     */
+    @Test
+    public void test_elimina_si_existeix() {
+        Assert.assertNotNull(operacions.carrega(1L));
+        operacions.elimina(1L);
+        Assert.assertNull(operacions.carrega(1L));
+        operacions.elimina(1L);
+    }
+
+    /**
+     * Implementa el metode modifica per a que guardi les modificacions
+     * de  l'objecte  passat com a parametre
+     */
+    @Test
+    public void test_modifica() {
+        Localitat modificada = new Localitat();
+        modificada.setId(1L);
+        modificada.setNom("modificada");
+
+        operacions.modifica(modificada);
+
+        Assert.assertEquals("modificada", operacions.carrega(1L).getNom());
+    }
 }
