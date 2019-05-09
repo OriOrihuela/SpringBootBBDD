d22a41bfb25a87498ce7954605acb0646976004f
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-09 20:31:28 +0200

    elimina() implemented; 5th test case passed.

--- 3_api_basica_exercici/src/main/java/org/formacio/api/LocalitatOpBasic.java
+++ 3_api_basica_exercici/src/main/java/org/formacio/api/LocalitatOpBasic.java
@@ -1,14 +1,18 @@
 package org.formacio.api;
 
+
 import org.springframework.stereotype.Component;
+
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 /**
  * Modifica aquesta classe per tal que sigui un component Spring que realitza les
  * operacions de persistencia tal com indiquen les firmes dels metodes
  */
+
 
 @Component
 public class LocalitatOpBasic {
@@ -23,16 +27,25 @@ public class LocalitatOpBasic {
         return entityManager;
     }
 
+
     /* ---- Behaviours ---- */
     public Localitat carrega(long id) {
         Localitat localitat = getEntityManager().find(Localitat.class, id);
         return localitat;
     }
 
+    @Transactional
     public void alta(String nom, Integer habitants) {
+        Localitat localitat = new Localitat();
+        localitat.setNom(nom);
+        localitat.setHabitants(habitants);
+        getEntityManager().persist(localitat);
     }
 
+    @Transactional
     public void elimina(long id) {
+        Localitat localitat = getEntityManager().find(Localitat.class, id);
+        getEntityManager().remove(localitat);
     }
 
     public void modifica(Localitat localitat) {
