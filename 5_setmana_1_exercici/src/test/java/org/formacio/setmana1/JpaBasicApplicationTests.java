4e5648c9af9268f6a0b63ce0084f54fa5e320e1b
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-15 20:03:57 +0200

    test_carrega_existent passed

--- 5_setmana_1_exercici/src/test/java/org/formacio/setmana1/JpaBasicApplicationTests.java
+++ 5_setmana_1_exercici/src/test/java/org/formacio/setmana1/JpaBasicApplicationTests.java
@@ -29,107 +29,106 @@ import org.springframework.test.web.servlet.MockMvc;
 @Sql("classpath:/insert.sql")
 public class JpaBasicApplicationTests {
 
-	@Autowired(required=false)
-	private LlibreOpsBasic operacions;
+    @Autowired(required = false)
+    private LlibreOpsBasic operacions;
 
     @Autowired
     private MockMvc mockMvc;
 
-	@PersistenceContext
-	private EntityManager em;
-	
-	private final String ISBN = "123456789";
-	
-	/**
-	 * Observa (sense tocar!) l'estructura de la taula per guardar localitats a:
-	 * src/main/resources/schema.sql
-	 * 
-	 * Modifica la classe Llibre per tal que es correspongui a la estructura de la taula
-	 */
-	@Test
-	public void test_mapping() {
-		em.find(Llibre.class, ISBN);
-	}
-	
-	/* ------------------------------
-	 * Les modificacions que el test demana a partir d'aqui es refereixen (a no ser que 
-	 * es digui d'altre manera) a la classe LlibresOpsBasic
-	 */
-	
-	/**
-	 * Modifica la classe LlibreOpsBasic per a que sigui localitzada com a Component per Spring
-	 */
-	@Test
-	public void test_component() {
-		Assert.assertNotNull(operacions);
-	}
-	
+    @PersistenceContext
+    private EntityManager em;
 
-	// -- Aneu implementant cada metode de LlibreOpsBasic que s'executa en cada test
-	
-	@Test
-	public void test_carrega_existent() throws LlibreNoExisteixException {
-		Assert.assertNotNull(operacions.carrega(ISBN));
-		Assert.assertEquals("Spring Boot in action", operacions.carrega(ISBN).getTitol());
-	}
+    private final String ISBN = "123456789";
+
+    /**
+     * Observa (sense tocar!) l'estructura de la taula per guardar localitats a:
+     * src/main/resources/schema.sql
+     * Modifica la classe Llibre per tal que es correspongui a la estructura de la taula
+     */
+    @Test
+    public void test_mapping() {
+        em.find(Llibre.class, ISBN);
+    }
+
+    /* ------------------------------
+     * Les modificacions que el test demana a partir d'aqui es refereixen (a no ser que
+     * es digui d'altre manera) a la classe LlibresOpsBasic
+     */
+
+    /**
+     * Modifica la classe LlibreOpsBasic per a que sigui localitzada com a Component per Spring
+     */
+    @Test
+    public void test_component() {
+        Assert.assertNotNull(operacions);
+    }
 
 
-	/**
-	 *  Aquest test comprova el cas especial de que quan el llibre no existeix es llanca una LlibreNoExisteixException 
-	 */
-	@Test(expected=LlibreNoExisteixException.class)
-	public void test_carrega_no_exitent() throws LlibreNoExisteixException {
-		operacions.carrega("xxxxx");
-		Assert.fail("Hauria d'haver llançat una excepcio");
-	}
+    // -- Aneu implementant cada metode de LlibreOpsBasic que s'executa en cada test
 
-	@Test
-	public void test_alta() {
-		operacions.alta("4444", "Joan Xarequet", 1000, Recomanacio.PRESCINDIBLE,
-				"Aplicacions sense Boot");
-		Llibre nou = em.find(Llibre.class, "4444");
-		Assert.assertNotNull(nou);
-		Assert.assertEquals(1000, nou.getPagines().intValue());
-		Assert.assertEquals(Recomanacio.PRESCINDIBLE, nou.getRecomanacio());
-	}
-	
-	@Test
-	public void test_elimina() {
-		Assert.assertNotNull(em.find(Llibre.class, ISBN));
-		Assert.assertTrue(operacions.elimina(ISBN));
-		Assert.assertNull(em.find(Llibre.class, ISBN));
-	}
-	
-	@Test
-	public void test_modifica() {
-		Llibre original = em.find(Llibre.class, ISBN);
-		original.setPagines(2000);
-		operacions.modifica(original);
-		Llibre modificat = em.find(Llibre.class, ISBN);
-		Assert.assertEquals(2000, modificat.getPagines().intValue());
-	}
-	
-	@Test
-	public void test_existeix() {
-		Assert.assertTrue(operacions.existeix(ISBN));
-		Assert.assertFalse(operacions.existeix("No existeix"));
-	}
-	
-	@Test
-	public void test_recomanacio() {
-		Assert.assertNull(operacions.recomenacioPer("No existeix"));
-		Assert.assertEquals(Recomanacio.OBRA_MESTRE, operacions.recomenacioPer(ISBN));
-	}
-	
-	/* ------------------------------
-	 * Ara que tot vos funciona, completeu el codi de LlibreController (package org.formacio.setmana1.mvc)
-	 */
-	
-	@Test
-	public void test_peticio_web() throws Exception {
+    @Test
+    public void test_carrega_existent() throws LlibreNoExisteixException {
+        Assert.assertNotNull(operacions.carrega(ISBN));
+        Assert.assertEquals("Spring Boot in action", operacions.carrega(ISBN).getTitol());
+    }
+
+
+    /**
+     * Aquest test comprova el cas especial de que quan el llibre no existeix es llanca una LlibreNoExisteixException
+     */
+    @Test(expected = LlibreNoExisteixException.class)
+    public void test_carrega_no_exitent() throws LlibreNoExisteixException {
+        operacions.carrega("xxxxx");
+        Assert.fail("Hauria d'haver llançat una excepcio");
+    }
+
+    @Test
+    public void test_alta() {
+        operacions.alta("4444", "Joan Xarequet", 1000, Recomanacio.PRESCINDIBLE,
+                "Aplicacions sense Boot");
+        Llibre nou = em.find(Llibre.class, "4444");
+        Assert.assertNotNull(nou);
+        Assert.assertEquals(1000, nou.getPagines().intValue());
+        Assert.assertEquals(Recomanacio.PRESCINDIBLE, nou.getRecomanacio());
+    }
+
+    @Test
+    public void test_elimina() {
+        Assert.assertNotNull(em.find(Llibre.class, ISBN));
+        Assert.assertTrue(operacions.elimina(ISBN));
+        Assert.assertNull(em.find(Llibre.class, ISBN));
+    }
+
+    @Test
+    public void test_modifica() {
+        Llibre original = em.find(Llibre.class, ISBN);
+        original.setPagines(2000);
+        operacions.modifica(original);
+        Llibre modificat = em.find(Llibre.class, ISBN);
+        Assert.assertEquals(2000, modificat.getPagines().intValue());
+    }
+
+    @Test
+    public void test_existeix() {
+        Assert.assertTrue(operacions.existeix(ISBN));
+        Assert.assertFalse(operacions.existeix("No existeix"));
+    }
+
+    @Test
+    public void test_recomanacio() {
+        Assert.assertNull(operacions.recomenacioPer("No existeix"));
+        Assert.assertEquals(Recomanacio.OBRA_MESTRE, operacions.recomenacioPer(ISBN));
+    }
+
+    /* ------------------------------
+     * Ara que tot vos funciona, completeu el codi de LlibreController (package org.formacio.setmana1.mvc)
+     */
+
+    @Test
+    public void test_peticio_web() throws Exception {
         this.mockMvc.perform(get("/recomanacio").param("isbn", ISBN)).andExpect(status().isOk())
-            .andExpect(content().string(containsString(Recomanacio.OBRA_MESTRE.toString())));
+                .andExpect(content().string(containsString(Recomanacio.OBRA_MESTRE.toString())));
 
-	}
+    }
 
 }
