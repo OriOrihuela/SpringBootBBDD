0a45f63bb3b450c4d0a683c1c7cb956428ce866a
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-15 20:38:52 +0200

    test_peticio_web passed

--- 5_setmana_1_exercici/src/main/java/org/formacio/setmana1/mvc/LlibreController.java
+++ 5_setmana_1_exercici/src/main/java/org/formacio/setmana1/mvc/LlibreController.java
@@ -1,28 +1,31 @@
 package org.formacio.setmana1.mvc;
 
+import org.formacio.setmana1.data.LlibreOpsBasic;
+import org.formacio.setmana1.domini.Llibre;
 import org.formacio.setmana1.domini.Recomanacio;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Completeu el codi per tal que la peticio /titol retorni el titol del llibre
  * amb l'isbn enviat com a parametre
- * 
+ * <p>
  * Aquesta classe ha de fer un de LlibreOpsBasic: No poseu aqui codi de persistencia!
  */
 
-@Controller
+@RestController
 public class LlibreController {
 
-	// Per aqui vos fara falta una referencia a un LlibreOpsBasic
-	
-	// Aquestes anotacions i firma del metode ja son correctes
-	@RequestMapping(path="/recomanacio")
-	@ResponseBody
- 	public Recomanacio obteLlibre (String isbn) {
-		// Feis que retorni la recomanacio per el llibre indicat 
-		// emprat LlibreOpsBasic
-		return null; 
-	}
+    // Per aqui vos fara falta una referencia a un LlibreOpsBasic
+    @Autowired
+    private LlibreOpsBasic llibreOpsBasic = null;
+
+    // Aquestes anotacions i firma del metode ja son correctes
+    @RequestMapping(path = "/recomanacio")
+    public Recomanacio obteLlibre(String isbn) {
+        // Feis que retorni la recomanacio per el llibre indicat
+        // emprat LlibreOpsBasic
+        return llibreOpsBasic.recomenacioPer(isbn);
+    }
 }
